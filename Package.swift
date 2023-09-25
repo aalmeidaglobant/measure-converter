a32a8c09481d2a34e0c376fb62298f65ae5e9855
aalmeidaglobant <akmere.almeida@globant.com>
2023-09-25 05:59:02 -0300

    remove packages

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MeasureConverter",
-            path: "./MeasureConverter.xcframework"
+            path: "./pods/debug/MeasureConverter.xcframework"
         ),
     ]
 )
